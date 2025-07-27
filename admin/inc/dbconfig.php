d6b2b076b989525f01c8959d6fbd93e53eb76688
AryanK20 <aryanrkushwah@gmail.com>
2025-07-27 13:06:48 +0530

    commit 1

---
+++ admin/inc/dbconfig.php
@@ -0,0 +1,69 @@
+<?php
+
+    $hname = 'localhost';
+    $uname = 'root';
+    $pass = '';
+    $db = 'akwebsite';
+    $port = 3307;
+
+    $con = new mysqli($hname, $uname, $pass, $db, $port);
+
+    if(!$con)
+    {
+        die("Cannot connect to Database".mysqli_connect_error());
+    }
+
+    function filteration($data)
+    {
+        foreach($data as $key => $value){
+            $data[$key] = trim($value);
+            $data[$key] = stripcslashes($value);
+            $data[$key] = htmlspecialchars($value);
+            $data[$key] = strip_tags($value);
+        }
+        return $data;
+    }
+
+    function select($sql, $values, $datatypes)
+    {
+        $con = $GLOBALS['con'];
+        if($stmt = mysqli_prepare($con,$sql)){
+            mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+            if(mysqli_stmt_execute($stmt)){
+                $res = mysqli_stmt_get_result($stmt);
+                mysqli_stmt_close($stmt);
+                return $res;
+            }
+            else{
+                mysqli_stmt_close($stmt);
+                die("Query cannot be executed - Select");
+            }
+        }
+        else{
+            die("Query cannot be prepared - Select");
+        }
+    }
+
+    
+    function update($sql, $values, $datatypes)
+    {
+        $con = $GLOBALS['con'];
+        if($stmt = mysqli_prepare($con,$sql)){
+            mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+            if(mysqli_stmt_execute($stmt)){
+                $res = mysqli_stmt_affected_rows($stmt);
+                mysqli_stmt_close($stmt);
+                return $res;
+            }
+            else{
+                mysqli_stmt_close($stmt);
+                die("Query cannot be executed - Update");
+            }
+        }
+        else{
+            die("Query cannot be prepared - Update");
+        }
+    }
+    
+
+?>
