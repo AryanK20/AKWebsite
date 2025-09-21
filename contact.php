12cb5fe3b73ae81ac97b5feaf73bc905a14e5d9d
AryanK20 <aryanrkushwah@gmail.com>
2025-09-21 22:58:22 +0530

    commit 2

--- contact.php
+++ contact.php
@@ -36,11 +36,11 @@
 
                     <h5 class="mt-4">Call us</h5>
                     <a href="tel: +91XXXXXXXXXX" class="d-inline-block mb-2 text-decoration-none text-dark">
-                        <i class="bi bi-telephone-fill"></i>+91XXXXXXXXXX
+                        <i class="bi bi-telephone-fill"></i>+917798254629
                     </a>
                     <br>
                     <a href="tel: +91XXXXXXXXXX" class="d-inline-block text-decoration-none text-dark">
-                        <i class="bi bi-telephone-fill"></i>+91XXXXXXXXXX
+                        <i class="bi bi-telephone-fill"></i>+918995846258
                     </a>
 
                     <h5 class="mt-4">Email</h5>
